592e61ca9a467d4e5e812c87c06e521ef126c9a0
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-09-28 12:08:37 +0800

    修改绘制bbox

--- ctpn/demo.py
+++ ctpn/demo.py
@@ -28,37 +28,9 @@ def resize_im(im, scale, max_scale=None):
     return iiimg, f
 
 
-def draw_boxes(img, image_name, boxes, scale):
-    base_name = image_name.split('/')[-1]
-    with open('data/results/' + 'res_{}.txt'.format(base_name.split('.')[0]), 'w') as f:
-        for box in boxes:
-            # if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
-            #     continue
-            # if abs(box[0] - box[1]) < 5 or abs(box[3] - box[0]) < 5:
-            #     continue
-
-            if box[8] >= 0.9:
-                color = (0, 255, 0)
-            elif box[8] >= 0.8:
-                color = (255, 0, 0)
-            else:
-                color = (0, 0, 255)
-            # cv2.line(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, 2)
-            # cv2.line(img, (int(box[0]), int(box[1])), (int(box[4]), int(box[5])), color, 2)
-            # cv2.line(img, (int(box[6]), int(box[7])), (int(box[2]), int(box[3])), color, 2)
-            # cv2.line(img, (int(box[4]), int(box[5])), (int(box[6]), int(box[7])), color, 2)
-            cv2.rectangle(img, (int(box[0]), int(box[1])),(int(box[2]), int(box[5])),color, 2)
-
-            min_x = min(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
-            min_y = min(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
-            max_x = max(int(box[0] / scale), int(box[2] / scale), int(box[4] / scale), int(box[6] / scale))
-            max_y = max(int(box[1] / scale), int(box[3] / scale), int(box[5] / scale), int(box[7] / scale))
-
-            line = ','.join([str(min_x), str(min_y), str(max_x), str(max_y)]) + '\r\n'
-            f.write(line)
-
-    img = cv2.resize(img, None, None, fx=1.0 / scale, fy=1.0 / scale, interpolation=cv2.INTER_LINEAR)
-    cv2.imwrite(os.path.join("data/results", base_name), img)
+def draw_boxes(img, boxes, color):
+    for box in boxes:
+        cv2.rectangle(img, (int(box[0]), int(box[1])),(int(box[2]), int(box[5])), color, 1)
 
 
 def ctpn(sess, net, image_path):
@@ -67,29 +39,46 @@ def ctpn(sess, net, image_path):
 
     img = cv2.imread(image_path)
     img_name = image_path.split('/')[-1]
-    # print('111', img.shape)
     #　将图像进行resize并返回其缩放大小
     img, scale = resize_im(img, scale=TextLineCfg.SCALE, max_scale=TextLineCfg.MAX_SCALE)
-    # print('222', img.shape)
     # 送入网络得到1000个得分,1000个bbox
     cls, scores, boxes = test_ctpn(sess, net, img)
 
+    print('cls, scores, boxes',cls.shape, scores.shape, boxes.shape)
+
     # img_re = img
     # for i in range(np.shape(boxes)[0]):
-    #     #print(np.shape(boxes))
     #     if cls[i] == 1:
-    #         color = (255,0,0)
+    #         color = (255, 0, 0)
     #     else:
-    #         color = (0,255,0)
+    #         color = (0, 255, 0)
     #     cv2.rectangle(img_re, (boxes[i][0],boxes[i][1]),(boxes[i][2],boxes[i][3]),color,1)
-    # print(img_name)
-    # cv2.imwrite(img_name, img_re)
-    # cv2.waitKey()
-    # assert o,'dwa'
+    # cv2.imwrite(os.path.join('./data/proposal_res', img_name), img_re)
 
-    textdetector = TextDetector()
-    boxes = textdetector.detect(boxes, scores[:, np.newaxis], img.shape[:2])
-    draw_boxes(img, image_path, boxes, scale)
+    handwritten_filter = np.where(cls==1)[0]
+    handwritten_scores = scores[handwritten_filter]
+    handwritten_boxes = boxes[handwritten_filter, :]
+
+    print_filter = np.where(cls==2)[0]
+    print_scores = scores[print_filter]
+    print_boxes = boxes[print_filter, :]
+
+    handwritten_detector = TextDetector()
+    handwritten_detector = TextDetector()
+
+    print('print_filter', np.array(print_filter).shape)
+    print('handwritten_boxes, handwritten_scores', handwritten_boxes.shape, handwritten_scores[:, np.newaxis].shape)
+
+    filted_handwritten_boxes = handwritten_detector.detect(handwritten_boxes, handwritten_scores[:, np.newaxis], img.shape[:2])
+    filted_print_boxes = handwritten_detector.detect(print_boxes, print_scores[:, np.newaxis], img.shape[:2])
+
+    # boxes = textdetector.detect(boxes, scores[:, np.newaxis], img.shape[:2])
+    draw_boxes(img, filted_handwritten_boxes, (255,0,0))
+    draw_boxes(img, filted_print_boxes, (0,255,0))
+
+    img = cv2.resize(img, None, None, fx=1.0 / scale, fy=1.0 / scale, interpolation=cv2.INTER_LINEAR)
+    cv2.imwrite(os.path.join("data/results", img_name), img)
+
     timer.toc()
     print(('Detection took {:.3f}s for '
            '{:d} object proposals').format(timer.total_time, boxes.shape[0]))
@@ -122,10 +111,8 @@ if __name__ == '__main__':
     # for i in range(2):
     #     _, _ = test_ctpn(sess, net, im)
 
-    im_names = glob.glob(os.path.join(cfg.DATA_DIR, 'demo', '*.png')) + \
-               glob.glob(os.path.join(cfg.DATA_DIR, 'demo', '*.jpg')) + \
-               glob.glob(os.path.join(cfg.DATA_DIR, 'demo', '*.JPG'))
-
+    im_names = glob.glob(os.path.join(cfg.DATA_DIR, 'demo', '*'))
+    # print(glob.glob(os.path.join(cfg.DATA_DIR, 'demo', '*.JPG')))
     for im_name in im_names:
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print(('Demo for {:s}'.format(im_name)))
